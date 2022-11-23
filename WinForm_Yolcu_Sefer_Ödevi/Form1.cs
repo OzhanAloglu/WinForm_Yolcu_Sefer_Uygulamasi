502443c5e843547e3e7f658b226eccc751e0f8ac
Ozhan <ozhanaloglu11@gmail.com>
2022-11-24 01:57:51 +0300

    Add project files.

---
+++ WinForm_Yolcu_Sefer_Ödevi/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Yolcu_Sefer_Ödevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text + "---- " + textBox2.Text + "---- " +
+                maskedTextBox1.Text + "---- " + maskedTextBox2.Text + "---- " + maskedTextBox3.Text + " ----" + 
+                maskedTextBox4.Text + "---- " + comboBox1.Text + " ----" + comboBox2.Text);
+        }
+    }
+}
